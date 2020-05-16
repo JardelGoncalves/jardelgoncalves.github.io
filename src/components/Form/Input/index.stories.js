285044eaa0e352fa25f09b48a20b25a589f36577
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-05-16 19:58:44 -0300

    docs: add Input component doc :pencil:

---
+++ src/components/Form/Input/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Input from '.';
+
+storiesOf('Input', module)
+  .add('Default', () => (
+    <Input placeholder="Informe seu nome" />
+  ));
