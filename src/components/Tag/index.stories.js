72e990fbd841926a266a84b83ee32ecf9bdb4784
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-05-16 22:12:44 -0300

    docs: add Tag Component doc :pencil:

---
+++ src/components/Tag/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Tag from '.';
+
+storiesOf('Tag', module)
+  .add('Default', () => (
+    <Tag text="React" />
+  ));
