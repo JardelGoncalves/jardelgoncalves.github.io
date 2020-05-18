d0f69a9dc040a1cd0248664d004b7d74a9a68bce
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-05-18 14:31:27 -0300

    feat: add TabBottom component doc :pencil:

---
+++ src/components/TabBottom/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import TabBottom from '.';
+
+storiesOf('TabBottom', module)
+  .add('Default', () => (
+    <TabBottom />
+  ));
