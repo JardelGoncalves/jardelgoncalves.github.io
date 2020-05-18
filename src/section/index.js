3ece0d7e04cecadeea578e5e3932d1c90a742c2e
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-05-18 00:44:56 -0300

    feat: export sections :sparkles:

---
+++ src/section/index.js
@@ -0,0 +1,15 @@
+import Hero from './Hero';
+import Skills from './Skills';
+import Services from './Services';
+import Projects from './Projects';
+import Timeline from './Timeline';
+import Contact from './Contact';
+
+export {
+  Hero,
+  Skills,
+  Services,
+  Projects,
+  Timeline,
+  Contact,
+};
