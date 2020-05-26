5513a33dc47803e0db274326c135112be0fae9f5
Jardel Gonçalves Ferreira <jardelgoncalves1996@gmail.com>
2020-05-25 22:34:47 -0300

    fet: add SEO :sparkles:

--- src/global/Layout/index.js
+++ src/global/Layout/index.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
 
 import GlobalStyle from '../GlobalStyle';
+import SEO from '../SEO';
 import { Wrapper } from './styles';
 
 const Layout = ({ children }) => (
@@ -30,6 +31,13 @@ const Layout = ({ children }) => (
             content="jBVmqPj11HBOz3mFElOh_pjysg3Anv-ON8UwyXTGPys"
           />
         </Helmet>
+        <SEO
+          lang="pt-BR"
+          description="Desenvolvedor Full-Stack e Mobile"
+          keywords="React, React Native, Node, Node.js, Javascript,
+          Desenvolvedor Frontend, Frontend, Desenvolvedor Backend,
+          Backend, MongoDB, PostgreSQL, TypeScript, Gatsby, Next.js"
+        />
         <GlobalStyle />
         <Wrapper>
           {children}
---
+++ src/global/SEO/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { StaticQuery, graphql } from 'gatsby';
+
+import ogImage from '../../assets/images/og-image.png';
+
+const detailsQuery = graphql`
+  query DefaultSEOQuery {
+    site {
+      siteMetadata {
+        title
+        author,
+      }
+    }
+  }
+`;
+
+const siteURL = 'https://jardelgoncalves.dev';
+
+const SEO = ({
+  description,
+  lang,
+  meta,
+  keywords,
+  title,
+}) => (
+  <StaticQuery
+    query={detailsQuery}
+    render={(data) => (
+      <Helmet
+        htmlAttributes={{ lang }}
+        title={title || data.site.siteMetadata.title}
+        titleTemplate={title && `%s | ${data.site.siteMetadata.title}`}
+        meta={[
+          { name: 'description', content: description },
+          { name: 'keywords', content: keywords },
+          { name: 'twitter:card', content: 'summary' },
+          { name: 'twitter:creator', content: data.site.siteMetadata.author },
+          { name: 'twitter:title', content: title },
+          { name: 'twitter:description', content: description },
+          { property: 'og:title', content: title },
+          { property: 'og:description', content: description },
+          { property: 'og:type', content: 'website' },
+          { property: 'og:url', content: siteURL },
+          { property: 'og:image', content: `${siteURL}${ogImage}` },
+          { property: 'og:image:alt', content: description },
+          { property: 'og:image:type', content: 'image/png' },
+          { property: 'og:image:width', content: '1062' },
+          { property: 'og:image:height', content: '719' },
+        ].concat(meta)}
+      />
+    )}
+  />
+);
+
+SEO.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  lang: PropTypes.string.isRequired,
+  keywords: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+};
+
+SEO.defaultProps = {
+  title: null,
+  description: '',
+  meta: [],
+  keywords: '',
+};
+
+export default SEO;
