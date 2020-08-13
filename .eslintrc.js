e5f52694153d6e6f88738bf6c411b86438bdea06
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-08-13 02:24:11 -0300

    feat: add new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,10 +19,19 @@ module.exports = {
     'react',
     '@typescript-eslint',
   ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', 'ts', 'tsx'] }],
     'react/jsx-props-no-spreading': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'react/require-default-props': 'off',
   },
 };
