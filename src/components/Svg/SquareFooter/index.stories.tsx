214b914c32741684911b27e1eab83a1e5b71c8db
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-08-13 02:13:43 -0300

    feat: add components svg , wave and squareFooter

---
+++ src/components/Svg/SquareFooter/index.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SquareFooter } from '.'
+export default {
+  title: 'Svg/SquareFooter',
+  component: SquareFooter,
+};
+
+export const Svg = () => <SquareFooter />;
+
+Svg.story = {
+  name: 'default',
+};
---
+++ src/components/Svg/Wave/index.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wave } from '.'
+export default {
+  title: 'Svg/Wave',
+  component: Wave,
+};
+
+export const Svg = () => <Wave />;
+
+Svg.story = {
+  name: 'default',
+};
