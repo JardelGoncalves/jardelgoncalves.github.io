e8e51528818045858620e2ddbecc0c35f94eae84
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-08-16 01:01:55 -0300

    feat: add font inter

--- src/components/GlobalStyle/index.ts
+++ src/components/GlobalStyle/index.ts
@@ -1,10 +1,27 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap');
+
   * {
     margin: 0;
     outline: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  a,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  span,
+  p,
+  body,
+  input,
+  button {
+    font-family: 'Inter', sans-serif;
   }
 `;
