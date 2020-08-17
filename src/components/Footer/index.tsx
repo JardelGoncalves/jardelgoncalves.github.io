6a92c373f041f00431d39822133c553e398e866f
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-08-17 00:30:38 -0300

    feat: footer finish

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,31 +1,72 @@
 import React from 'react';
 
-import { Wave, SquareFooter } from '../Svg';
+import {
+  Wave,
+  SquareFooter,
+  GithubIcon,
+  LinkedinIcon,
+  MediumIcon,
+  MailIcon,
+  TelegramIcon,
+  WhatsAppIcon,
+} from '../Svg';
+import { IconButton } from '../Button';
 import { Title } from '../Title';
 
-import { Wrap, Row } from './styles';
+import { Wrap, Row, ListItems } from './styles';
+
+const config = {
+  follow: [
+    { icon: <GithubIcon />, link: 'https://github.com/jardelgoncalves' },
+    { icon: <LinkedinIcon />, link: 'https://linkedin.com/in/jardelgoncalves' },
+    { icon: <MediumIcon />, link: 'https://medium.com/@jardelgoncalves' },
+  ],
+  contacts: [
+    { icon: <MailIcon />, name: 'jardelgoncalves1996@gmail.com', link: 'mailto:jardelgoncalves1996@gmail.com' },
+    { icon: <TelegramIcon />, name: 'Telegram', link: 'https://t.me/jardelgoncalves' },
+    { icon: <WhatsAppIcon />, name: 'WhatsApp', link: 'https://api.whatsapp.com/send?phone=5588981236663&text=Ol%C3%A1%20Jardel' },
+  ],
+};
 
 export const Footer: React.FC = () => (
   <Wrap>
-    <Wave />
-    <SquareFooter />
     <Row>
       <div className="column">
-        <Title size={60} color="#ffffff" text="Contatos" />
-        <ul>
-          <li>jardelgoncalves1996@gmail.com</li>
-          <li>Telegram</li>
-          <li>WhatsApp</li>
-        </ul>
+        <Title disableSvg={false} size={60} color="#ffffff" text="Contatos" />
+        <ListItems>
+          {config.contacts.map((c:any) => (
+            <li key={c.link}>
+              <a
+                className="item"
+                href={c.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {c.icon}
+                {c.name || c.link}
+              </a>
+            </li>
+
+          ))}
+        </ListItems>
       </div>
       <div className="column">
-        <Title size={60} color="#ffffff" text="Siga-me" />
-        <ul>
-          <li>Linkedin</li>
-          <li>Github</li>
-          <li>Medium</li>
-        </ul>
+        <Title disableSvg={false} size={60} color="#ffffff" text="Siga-me" />
+        <ListItems row>
+          {config.follow.map((f:any) => (
+            <li key={f.link}>
+              <IconButton
+                path={f.link}
+                bg="#fff"
+                color="#5BCA5D"
+                icon={f.icon}
+              />
+            </li>
+          ))}
+        </ListItems>
       </div>
     </Row>
+    <SquareFooter />
+    <Wave />
   </Wrap>
 );
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -6,7 +6,6 @@ export const Wrap = styled.div`
   background-color: ${({ theme }) => theme.colors.primary};
   margin-top: 100px;
   position: relative;
-  z-index: -1;
 
 
   svg.wave {
@@ -21,20 +20,53 @@ export const Wrap = styled.div`
     position: absolute;
     bottom: 0;
     right: 0;
-    z-index: -1;
+    z-index: 0;
   }
 `;
 
 export const Row = styled.div`
+  z-index: 1;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  align-items: center;
-  z-index: 2;
-  padding: 60px;
+  align-items: flex-start;
+  padding: 100px 60px;
 
   div.column {
+    z-index: 1;
     display: flex;
     flex-direction: column;
+  }
+`;
+
+interface IListItems {
+  row?: boolean | false;
+}
+
+export const ListItems = styled.ul<IListItems>`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-direction: ${({ row }) => (row ? 'row' : 'column')};
+
+  li {
+    padding: 12px 0;
+    padding-right: 20px;
+    display: flex;
+    align-items: center;
+
+    a.item {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+      font-size: 18px;
+      color: #fff;
+      cursor: pointer;
+
+      svg {
+        margin-right: 12px;
+      }
+    }
   }
 `;
