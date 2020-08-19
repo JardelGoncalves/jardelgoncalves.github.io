32dab95920ad775923d3da3f41cb4d68ab099985
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-08-19 00:41:43 -0300

    feat: add header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import Headroom from 'react-headroom';
+
+export const Wrapper = styled(Headroom)`
+  .headroom--pinned {
+    background: ${({ theme }) => theme.colors.primary};
+    -webkit-box-shadow: 0px 23px 28px -30px rgba(0,0,0,0.52);
+    -moz-box-shadow: 0px 23px 28px -30px rgba(0,0,0,0.52);
+    box-shadow: 0px 23px 28px -30px rgba(0,0,0,0.52);
+  };
+
+  .headroom {
+    transition: all 0.2s ease-in-out;
+  }
+  
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  header {
+    height: 80px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 80px;
+
+    .logo svg {
+      width: 70px;
+      height: auto;
+    }
+    ul {
+      display: flex;
+      flex-direction: row;
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      li {
+        margin: 0 20px;
+        a {
+          text-decoration: none;
+          font-size: 16px;
+          color: #fff;
+          font-weight: 700;
+          padding: 8px 6px;
+          border-bottom: 2px solid transparent;
+          &:hover {
+            border-bottom: 2px solid #fff;
+            transition: all 0.2s ease-in-out;
+          }
+        }
+      }
+    }
+  }
+`;
