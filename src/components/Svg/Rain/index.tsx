a132c6d4f732d21d8d4016bfaa62365f43b297ee
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-08-19 11:31:46 -0300

    feat: add rain component

---
+++ src/components/Svg/Rain/index.tsx
@@ -0,0 +1,308 @@
+import React from 'react';
+
+export const Rain = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1110"
+    height="377"
+    fill="none"
+    viewBox="0 0 1110 377"
+  >
+    <path
+      stroke="url(#paint0_linear)"
+      d="M97.536 157.583l-45.278-45.349"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M100.705 155.403H105.593V160.291H100.705z"
+      transform="rotate(86.54 100.705 155.403)"
+    />
+    <path
+      stroke="url(#paint1_linear)"
+      d="M277.278 182.73L232 137.382"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M280.447 180.511H285.335V185.399H280.447z"
+      transform="rotate(86.54 280.447 180.511)"
+    />
+    <path
+      stroke="url(#paint2_linear)"
+      d="M257.278 46.518L212 1.17"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M260.447 44.511H265.335V49.399H260.447z"
+      transform="rotate(86.54 260.447 44.511)"
+    />
+    <path
+      stroke="url(#paint3_linear)"
+      d="M476.278 108.615L431 63.267"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M479.447 106.511H484.335V111.399H479.447z"
+      transform="rotate(86.54 479.447 106.511)"
+    />
+    <path
+      stroke="url(#paint4_linear)"
+      d="M500.278 287.894L455 242.546"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M503.447 285.511H508.335V290.399H503.447z"
+      transform="rotate(86.54 503.447 285.511)"
+    />
+    <path
+      stroke="url(#paint5_linear)"
+      d="M728.278 200.758L683 155.41"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M731.447 198.511H736.335V203.399H731.447z"
+      transform="rotate(86.54 731.447 198.511)"
+    />
+    <path
+      stroke="url(#paint6_linear)"
+      d="M847.278 128.646L802 83.298"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M850.447 126.511H855.335V131.399H850.447z"
+      transform="rotate(86.54 850.447 126.511)"
+    />
+    <path
+      stroke="url(#paint7_linear)"
+      d="M966.278 314.936L921 269.588"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M969.447 312.511H974.335V317.399H969.447z"
+      transform="rotate(86.54 969.447 312.511)"
+    />
+    <path
+      stroke="url(#paint8_linear)"
+      d="M1057.28 177.723L1012 132.374"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M1060.45 175.511H1065.338V180.399H1060.45z"
+      transform="rotate(86.54 1060.45 175.511)"
+    />
+    <path
+      stroke="url(#paint9_linear)"
+      d="M1106.28 318.942L1061 273.594"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M1109.45 316.511H1114.338V321.399H1109.45z"
+      transform="rotate(86.54 1109.45 316.511)"
+    />
+    <path
+      stroke="url(#paint10_linear)"
+      d="M567.278 374.028L522 328.68"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M570.447 371.511H575.335V376.399H570.447z"
+      transform="rotate(86.54 570.447 371.511)"
+    />
+    <path
+      stroke="url(#paint11_linear)"
+      d="M195.278 313.934L150 268.586"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M198.447 311.511H203.335V316.399H198.447z"
+      transform="rotate(86.54 198.447 311.511)"
+    />
+    <path
+      stroke="url(#paint12_linear)"
+      d="M303.278 313.934L258 268.586"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M306.447 311.511H311.335V316.399H306.447z"
+      transform="rotate(86.54 306.447 311.511)"
+    />
+    <path
+      stroke="url(#paint13_linear)"
+      d="M46.278 251.838L1 206.49"
+      opacity="0.3"
+    />
+    <path
+      fill="#5BCA5D"
+      d="M49.447 249.511H54.335V254.399H49.447z"
+      transform="rotate(86.54 49.447 249.511)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear"
+        x1="104.38"
+        x2="62.92"
+        y1="164.438"
+        y2="123.726"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear"
+        x1="284.122"
+        x2="242.662"
+        y1="189.585"
+        y2="148.874"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+      <linearGradient
+        id="paint2_linear"
+        x1="264.122"
+        x2="222.662"
+        y1="53.373"
+        y2="12.662"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+      <linearGradient
+        id="paint3_linear"
+        x1="483.122"
+        x2="441.662"
+        y1="115.47"
+        y2="74.758"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+      <linearGradient
+        id="paint4_linear"
+        x1="507.122"
+        x2="465.662"
+        y1="294.749"
+        y2="254.037"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+      <linearGradient
+        id="paint5_linear"
+        x1="735.122"
+        x2="693.662"
+        y1="207.613"
+        y2="166.902"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+      <linearGradient
+        id="paint6_linear"
+        x1="854.122"
+        x2="812.662"
+        y1="135.501"
+        y2="94.79"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+      <linearGradient
+        id="paint7_linear"
+        x1="973.122"
+        x2="931.662"
+        y1="321.791"
+        y2="281.08"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+      <linearGradient
+        id="paint8_linear"
+        x1="1064.12"
+        x2="1022.66"
+        y1="184.577"
+        y2="143.866"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+      <linearGradient
+        id="paint9_linear"
+        x1="1113.12"
+        x2="1071.66"
+        y1="325.797"
+        y2="285.086"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+      <linearGradient
+        id="paint10_linear"
+        x1="574.122"
+        x2="532.662"
+        y1="380.883"
+        y2="340.172"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+      <linearGradient
+        id="paint11_linear"
+        x1="202.122"
+        x2="160.662"
+        y1="320.789"
+        y2="280.078"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+      <linearGradient
+        id="paint12_linear"
+        x1="310.122"
+        x2="268.662"
+        y1="320.789"
+        y2="280.078"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+      <linearGradient
+        id="paint13_linear"
+        x1="53.122"
+        x2="11.662"
+        y1="258.693"
+        y2="217.981"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#5BCA5D" />
+        <stop offset="1" stopColor="#5BCA5D" stopOpacity="0.08" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
--- src/components/Svg/index.ts
+++ src/components/Svg/index.ts
@@ -2,3 +2,4 @@ export * from './Wave';
 export * from './SquareFooter';
 export * from './Icons';
 export * from './Logo';
+export * from './Rain';
