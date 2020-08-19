a6c14396548fe653e06743ccd79276ff6e8fe148
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-08-19 14:44:58 -0300

    feat: add animation type write

--- src/components/Hello/index.tsx
+++ src/components/Hello/index.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { init } from 'ityped';
 
 import { Wrap } from './styles';
 import { Rain } from '../Svg';
 
-export const Hello = () => (
-  <Wrap className="typewriter">
-    <Rain />
-    <h1>
-      Hello
-      <span className="text-primary">,</span>
-    </h1>
-    <h2>
-      My name is
-      {' '}
-      <span className="text-primary">Jardel Gonçalves</span>
-    </h2>
-    <span className="office">I&apos;m a Full Stack Developer</span>
-  </Wrap>
-);
+export const Hello = () => {
+  useEffect(() => {
+    const myElement = document.querySelector('#my-name');
+    init(myElement, {
+      showCursor: false,
+      strings: ['Jardel Gonçalves'],
+      loop: false,
+      typeSpeed: 80,
+    });
+  }, []);
+
+  return (
+    <Wrap>
+      <Rain />
+      <h1>
+        Hello
+        <span className="text-primary">,</span>
+      </h1>
+      <h2>
+        My name is
+        {' '}
+        <span className="text-primary" id="my-name" />
+      </h2>
+      <span className="office">I&apos;m a Full Stack Developer</span>
+    </Wrap>
+  );
+};
