b9667257f02b4500940d50a818d3fc1b27edd74b
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-08-31 23:32:40 -0300

    feat: about me responsible

--- src/containers/About/styles.ts
+++ src/containers/About/styles.ts
@@ -43,4 +43,36 @@ export const Wrap = styled.div`
       }
     }
   }
+
+  h1,
+  .about__content {
+    transition: all 0.2s ease-in-out;
+  }
+
+
+  @media only screen and (max-width: 950px) {
+    flex-direction: column-reverse;
+    padding-left: 60px;
+    padding-right: 60px;
+
+    .about__content {
+      margin-top: 50px;
+    }
+  }
+
+  @media only screen and (max-width: 600px) {
+    padding-left: 40px;
+    padding-right: 40px;
+    h1 {
+      font-size: 50px;
+    }
+  }
+
+  @media only screen and (max-width: 420px) {
+    padding-left: 20px;
+    padding-right: 20px;
+    h1 {
+      font-size: 40px;
+    }
+  }
 `;
