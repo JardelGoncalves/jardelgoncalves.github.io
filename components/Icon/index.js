ad454e2d904578c2b839041a52cdf17c78a1d4ee
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-01-23 22:05:49 -0300

    refactor: move fill color to parent element

--- components/Icon/index.js
+++ components/Icon/index.js
@@ -5,11 +5,13 @@ import { Container } from './styles';
 import Linkedin from './svg/linkedin.js'
 import Email from './svg/email.js'
 import Github from './svg/github.js'
+import Moon from './svg/moon.js'
 
 const icons = {
   github: <Github />,
   linkedin: <Linkedin />,
-  email: <Email />
+  email: <Email />,
+  moon: <Moon />
 }
 
 export const Icon = ({ name, size }) => {
