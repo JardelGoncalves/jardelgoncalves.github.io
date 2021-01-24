1591b260a016ce894f0156a25c657a46d9cf9bfd
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-01-24 11:54:01 -0300

    feat add home page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Navbar } from '../components/Navbar'
+import { GlobalStyle } from '../components/GlobalStyle';
+
+const Home = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Navbar />
+    </>
+  )
+}
+export default Home
