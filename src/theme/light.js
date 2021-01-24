e66ea3e468dfed5d4c8323510cefd8d3ac7789f8
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-01-24 15:48:02 -0300

    feat: add light theme

---
+++ src/theme/light.js
@@ -0,0 +1,12 @@
+import { base } from "./base";
+
+export const light = base({
+  '--bg-color': '#ffffff',
+  '--navbar-border-color': '#f0f0f0',
+  '--title-color': '#3d3d40',
+  '--subtitle-color': '#7C7C7C',
+  '--text-color': '#616469',
+  '--primary-color': '#38EF7D',
+  '--secondary-color': '#11998E',
+  '--slider-bg-color': '#616469',
+})
