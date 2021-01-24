56bf1ce7a39373df43189ebfe4fa55b4ad6d572f
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-01-24 15:48:12 -0300

    feat: add dark theme

---
+++ src/theme/dark.js
@@ -0,0 +1,12 @@
+import { base } from "./base";
+
+export const dark = base({
+  '--bg-color': '#333',
+  '--navbar-border-color': '#444',
+  '--title-color': '#ffffff',
+  '--subtitle-color': '#aaaaaa',
+  '--text-color': '#dddddd',
+  '--primary-color': '#38EF7D',
+  '--secondary-color': '#11998E',
+  '--slider-bg-color': '#616469',
+})
