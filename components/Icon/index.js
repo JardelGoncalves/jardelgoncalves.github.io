9cfbd26096d22857af42bceb5855349c0235b4cc
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-01-24 11:53:35 -0300

    feat: add logo icon

--- components/Icon/index.js
+++ components/Icon/index.js
@@ -6,12 +6,14 @@ import Linkedin from './svg/linkedin.js'
 import Email from './svg/email.js'
 import Github from './svg/github.js'
 import Moon from './svg/moon.js'
+import Logo from './svg/logo.js'
 
 const icons = {
+  logo: <Logo />,
   github: <Github />,
   linkedin: <Linkedin />,
   email: <Email />,
-  moon: <Moon />
+  moon: <Moon />,
 }
 
 export const Icon = ({ name, size }) => {
