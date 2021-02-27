b53f9483620e7972cc1e2642bd0766be13a53c11
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-02-27 02:36:11 -0300

    feat: add gradient colors

--- src/theme/base.ts
+++ src/theme/base.ts
@@ -5,5 +5,7 @@ export interface Colors {
 export const base = (colors?: Colors): Colors => ({
   '--bg-color': '#FAFAFA',
   '--text-color': '#050505',
+  '--gradient-from': '#f59c16',
+  '--gradient-to': '#eb0774',
   ...(colors || {})
 })
