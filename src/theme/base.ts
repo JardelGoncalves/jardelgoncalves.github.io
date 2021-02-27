498582ee2cecfabe8a569aa7244c2f87321eb9b4
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-02-26 23:49:16 -0300

    feat: creating base theme and exports themes

---
+++ src/theme/base.ts
@@ -0,0 +1,9 @@
+export interface Colors {
+  [key: string]: string
+}
+
+export const base = (colors?: Colors): Colors => ({
+  'bg-color': '#FAFAFA',
+  'text-color': '#050505',
+  ...(colors || {})
+})
