be2c27410adb871a7456d8e9b1da276d833f0d55
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-03-01 23:48:47 -0300

    feat: add scroll animation

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useRef } from 'react'
 import { Icon } from '../Icon'
 import { SwitchDarkMode } from '../SwitchDarkMode'
 import { Wrapper } from './styles'
 
 export const Navbar = () => {
+  const ref = useRef<HTMLElement>(null)
+
+  useEffect(() => {
+    window.onscroll = () => {
+      if (ref && ref.current) {
+        if (
+          document.body.scrollTop > 50 ||
+          document.documentElement.scrollTop > 50
+        ) {
+          ref.current.classList.add('navbar--fixed')
+        } else {
+          ref.current.classList.remove('navbar--fixed')
+        }
+      }
+    }
+  }, [])
   return (
-    <Wrapper>
+    <Wrapper ref={ref}>
       <Icon name="logo" />
       <SwitchDarkMode />
     </Wrapper>
