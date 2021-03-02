d399b01f06381e683377a5a946dd555242505b6a
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-03-01 23:49:07 -0300

    feat: add animetion and navbar fixed style

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -1,16 +1,36 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const fixedNavbar = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
 
 export const Wrapper = styled.nav`
   height: 70px;
-  width: 100vw;
-  border-bottom: 1px solid var(--border-color);
-  position: fixed;
+  width: 100%;
   display: flex;
-  top: 0;
-  left: 0;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
   padding: 0 90px;
+  position: relative;
+  border-bottom: 1px solid var(--border-color);
   transition: border 0.4s;
+  z-index: 4;
+
+  &.navbar--fixed {
+    animation: ${fixedNavbar} 0.5s;
+    position: fixed;
+    top: 0;
+    left: 0;
+    backdrop-filter: saturate(180%) blur(20px);
+  }
+
+  @media only screen and (max-width: 720px) {
+    padding: 0 32px;
+  }
 `
