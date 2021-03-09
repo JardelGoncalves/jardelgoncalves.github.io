616d209bd2b667cf3be2a442f5a661313e033327
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-03-08 23:45:05 -0300

    feat: add about page

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,39 @@
+import { Navbar } from '../../components/Navbar'
+import { Container } from '../../components/Container'
+
+import { Content, Paragraph } from './styles'
+
+export default function About() {
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <Content>
+          <Paragraph>
+            I am a software developer currently living in Quixadá/Brazil.
+          </Paragraph>
+
+          <Paragraph>
+            Right now, I&apos;m a back-end developer at LinkApi. I am also a
+            founder and mobile and web developer at Help4Pet, a platform to
+            generate QR Code that can be used on your pet collar to make it
+            easier for others to find you if your pet disappears.
+          </Paragraph>
+
+          <Paragraph>
+            I have a degree in Computer Networks at the Federal University of
+            Ceara and I am currently studying Computer Science at the University
+            Cruzeiro do Sul.
+          </Paragraph>
+
+          <Paragraph>
+            I work with technologies like React.js, Next.js, GraphQL, React
+            Native, Vue.js, Flutter, Typescript, Javascript, Node.js, Docker,
+            MongoDB, PostgreSQL, MySQL, AWS S3, AWS Lambda, AWS SQS, AWS ECS,
+            AWS EC2, DynamoDB, SASS and Css-in -JS.
+          </Paragraph>
+        </Content>
+      </Container>
+    </>
+  )
+}
