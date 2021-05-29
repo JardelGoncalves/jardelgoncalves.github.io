64cb0fc44db4c157c67652e830213f2da9d57476
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-05-29 00:40:40 -0300

    refactor: change text to pt-br

--- src/containers/Hero/index.tsx
+++ src/containers/Hero/index.tsx
@@ -4,12 +4,12 @@ import { SocialButton } from '../../components/Buttons'
 export const Hero = () => {
   return (
     <Wrapper>
-      <Title>Hey, I&apos;m Jardel</Title>
+      {/* <Title>Hey, I&apos;m Jardel</Title> */}
+      <Title>Hey, eu sou o Jardel</Title>
       <Description>
-        I&apos;m a software developer and writer, living in Quixadá/Brazil.
-        I&apos;m currently building web and mobile applications on personal
-        projects and I work as a <strong>Front-End</strong> at{' '}
-        <strong>Ioasys</strong>.
+        Eu sou desenvolvedor de software, moro em Quixadá/Brasil. Eu atualmente
+        desenvolvo aplicações Web e Mobile em projetos pessoais e trabalho como
+        desenvolvedor <strong>Front-End</strong> at <strong>Ioasys</strong>
       </Description>
       <SocialList>
         <SocialButton
