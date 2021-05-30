c4e091efee15abd794cda2b286b02bbd85463ce9
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-05-30 20:55:17 -0300

    refactor:  change style to using theme variable

--- src/components/core/Navbar/styles.ts
+++ src/components/core/Navbar/styles.ts
@@ -1,67 +1,67 @@
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
 
-export const Wrapper = styled(motion.nav)`
+export const Container = styled(motion.nav)`
+  position: fixed;
   height: ${({ theme }) => theme.sizes.desktop.navbar};
   width: 100%;
+  backdrop-filter: blur(15px);
+  border-bottom: 1px solid ${({ theme }) => theme.colors.borderColor};
+  transition: border 0.4s;
+  transition: padding 0.2s;
+  z-index: 9999;
+  display: flex;
+  align-items: center;
+`
+
+export const NavWrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  padding: 0.25rem 5.5rem;
-  position: fixed;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.borderColor};
-  backdrop-filter: blur(10px);
-  transition: border 0.4s;
-  transition: padding 0.2s;
-  z-index: 9999;
-  background-color: ${({ theme }) => theme.colors.bgColor};
+  width: 100%;
 
   img {
     width: auto;
     height: 3.2rem;
   }
+`
 
-  .menu {
-    display: flex;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    align-items: center;
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`
 
-    &__item {
-      color: ${({ theme }) => theme.colors.menuTextColor};
-      font-weight: 700;
-      padding: 8px 24px;
-      position: relative;
+export const NavMenuItem = styled.li`
+  color: ${({ theme }) => theme.colors.menuTextColor};
+  font-weight: ${({ theme }) => theme.typography.weight.medium};
+  padding: 0.5rem 1.5rem;
+  position: relative;
 
-      a {
-        &::after {
-          content: '';
-          background-color: var(--primary-color);
-          position: absolute;
-          bottom: 0;
-          left: 0;
-          right: 0;
-          margin: 0 auto;
-          height: 4px;
-          border-radius: 2px;
-          width: 0%;
-          transition: 0.2s;
-        }
-        &:hover,
-        &::after {
-          color: var(--primary-color);
-        }
+  a {
+    &::after {
+      content: '';
+      background-color: ${({ theme }) => theme.colors.primary900};
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      margin: 0 auto;
+      height: 0.25rem;
+      border-radius: 0.125rem;
+      width: 0%;
+      transition: 0.2s;
+    }
+    &:hover,
+    &::after {
+      color: ${({ theme }) => theme.colors.primary900};
+    }
 
-        &:hover::after {
-          width: 35%;
-        }
-      }
+    &:hover::after {
+      width: 35%;
     }
   }
 `
-/* @media only screen and (max-width: ${BREAKPOINT_SM}) {
-    transition: padding 0.2s;
-    padding: 0 32px;
-  } */
