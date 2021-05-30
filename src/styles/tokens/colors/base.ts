ed734d3308ea614cae84907b2f7f8dc250ac2d54
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-05-29 23:39:06 -0300

    feat: add new structure to color system

---
+++ src/styles/tokens/colors/base.ts
@@ -0,0 +1,46 @@
+export interface Colors {
+  [key: string]: string
+}
+
+export const base = (colors?: Colors): Colors => ({
+  white: '#FFFFFF',
+  grayscale50: '#F4F4F5',
+  grayscale100: '#E8E8E8',
+  grayscale200: '#DBDDE0',
+  grayscale300: '#CACCCF',
+  grayscale400: '#A0A4A8',
+  grayscale500: '#52575C',
+  grayscale900: '#25282B',
+  primary50: '#E5F2FF',
+  primary100: '#CCE5FF',
+  primary200: '#B3D8FF',
+  primary300: '#99CBFF',
+  primary400: '#80BFFF',
+  primary500: '#66B2FF',
+  primary600: '#4DA5FF',
+  primary700: '#3398FF',
+  primary800: '#1A8BFF',
+  primary900: '#007EFF',
+  warning500: '#FFBC1F',
+  warning700: '#F6A609',
+  warning900: '#E89806',
+  success500: '#40DD7F',
+  success700: '#2AC769',
+  success900: '#1AB759',
+  negative500: '#FF6262',
+  negative700: '#FB4E4E',
+  negative900: '#E93C3C',
+
+  bgColor: '#FAFAFA',
+  textColor: '#25282B',
+  textGradientFrom: '#FB4E4E',
+  textGradientTo: '#2AC769',
+  borderColor: '#E8E8E8',
+  sliderBgColor: '#52575C',
+  menuTextColor: '#52575C',
+  switchModeBorder: 'rgba(97, 100, 105, 0.2)',
+  switchModeBg: 'transparent',
+  ...(colors || {})
+})
+
+/* Color styles */
---
+++ src/styles/tokens/colors/dark.ts
@@ -0,0 +1,9 @@
+import { Colors, base } from './base'
+export const dark: Colors = base({
+  bgColor: '#25282B',
+  textColor: '#FAFAFA',
+  borderColor: '#e4e4e71a',
+  menuTextColor: '#FAFAFA',
+  switchModeBorder: 'rgba(0, 126, 255, 0.3)',
+  switchModeBg: 'rgba(0, 126, 255, 0.2)'
+})
