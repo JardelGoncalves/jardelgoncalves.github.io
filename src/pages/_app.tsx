257e4b498c1903ae6180fb034f5570edd12f4f96
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-05-29 23:42:02 -0300

    feat: add logic to switch theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
 import Head from 'next/head'
-import { Navbar } from '../components/Navbar'
+
+import { Navbar } from '../components/core/Navbar'
+import { SwitchDarkMode } from '../components/core/SwitchDarkMode'
+import { THEME_SETTING_KEY } from '../utils/constants/local-storage'
+import { THEMES } from '../utils/constants/enums'
+
+import theme from '../styles/theme'
 import '../styles/globals.css'
-import { Providers } from '../utils/providers'
+import { Cache } from '../utils/cache'
 
 type Props = {
   Component: React.FC
@@ -10,14 +18,38 @@ type Props = {
 }
 
 function MyApp({ Component, pageProps }: Props) {
+  console.log(Cache.get(THEME_SETTING_KEY))
+  const [isDark, setIsDark] = useState(
+    Cache.get(THEME_SETTING_KEY) === THEMES.DARK
+  )
+
+  const toggle = () => {
+    const mode = Cache.get(THEME_SETTING_KEY)
+    console.log(mode)
+    if (mode === THEMES.DARK) {
+      Cache.set(THEME_SETTING_KEY, THEMES.LIGHT)
+      setIsDark(false)
+      return
+    }
+    Cache.set(THEME_SETTING_KEY, THEMES.DARK)
+    setIsDark(true)
+  }
+
   return (
-    <Providers>
+    <ThemeProvider
+      theme={{
+        ...theme,
+        colors: isDark ? theme.colors.dark : theme.colors.light
+      }}
+    >
       <Head>
         <title>Jardel Gonçalves</title>
       </Head>
-      <Navbar />
+      <Navbar>
+        <SwitchDarkMode isDark={isDark} toggle={toggle} />
+      </Navbar>
       <Component {...pageProps} />
-    </Providers>
+    </ThemeProvider>
   )
 }
 
