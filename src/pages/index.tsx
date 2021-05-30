1f44dca250711ca07ddebbfdee9e199cf1cb020a
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-05-30 20:45:39 -0300

    refactor: move page to container and add gatstaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
-// import { Hero } from '../containers/Hero'
-export default function Home() {
-  return (
-    <>
-      {/* <Hero /> */}
-      <h1>Hello</h1>
-    </>
-  )
+import { GetStaticProps } from 'next'
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      metaDescription: '',
+      metaType: '',
+      pageTitle: 'Jardel Goncalves | Blog`s'
+    },
+    revalidate: 60 * 60 * 24 // 1 day
+  }
 }
+
+export { default } from '../containers/Home'
