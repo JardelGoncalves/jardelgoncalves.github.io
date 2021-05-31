616d774fa45548bf07e5c99ed354c20f01ad8ee3
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-05-30 23:06:40 -0300

    refactor: change page title and add meta description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,10 @@ import { GetStaticProps } from 'next'
 export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
-      metaDescription: '',
+      metaDescription:
+        'Sou desenvolvedor Front-End, tenho experiência também com tecnlogias de Back-End como Node.js. Este site foi desenvolvido com o intuito de ajudar pessoas que querem entrar no mundo da programação.',
       metaType: '',
-      pageTitle: 'Jardel Goncalves | Blog`s'
+      pageTitle: 'Jardel Goncalves'
     },
     revalidate: 60 * 60 * 24 // 1 day
   }
