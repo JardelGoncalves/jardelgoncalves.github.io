040faf61630b3fd791ca7e30c642e2c10a86f2a2
jardel <jardelgoncalves1996@gmail.com>
2021-06-09 23:00:02 -0300

    refactor: change prop

--- src/components/core/Icon/styles.ts
+++ src/components/core/Icon/styles.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
-interface IProps {
-  size?: number
+interface WrapperProps {
+  size?: number | undefined
 }
 
-export const Wrapper = styled.div<IProps>`
+export const Wrapper = styled.div<WrapperProps>`
   svg {
     width: ${({ size }) => `${size || 32}px`};
     height: ${({ size }) => `${size || 32}px`};
