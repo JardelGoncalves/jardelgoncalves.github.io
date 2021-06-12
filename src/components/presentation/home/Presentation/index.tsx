84f01df9c93e531c299099c7febb133cd91ae8e9
jardel <jardelgoncalves1996@gmail.com>
2021-06-11 22:40:37 -0300

    feat: create presentation component

---
+++ src/components/presentation/home/Presentation/index.tsx
@@ -0,0 +1,31 @@
+import { SocialButton } from 'components/core/Button'
+
+import * as S from './styles'
+
+export default function Presentation() {
+  return (
+    <S.PresentationContainer>
+      <S.TitleColorize>Hey, eu sou o Jardel</S.TitleColorize>
+      <S.Description>
+        Eu sou desenvolvedor de software, moro em Quixadá/Brasil. Eu atualmente
+        desenvolvo aplicações Web e Mobile em projetos pessoais e trabalho como
+        desenvolvedor <strong>Front-End</strong> na <strong>Ioasys</strong>
+      </S.Description>
+      <S.SocialList>
+        <SocialButton
+          icon="github"
+          url="https://github.com/jardelgoncalves"
+          target="_blank"
+        />
+
+        <SocialButton
+          icon="linkedin"
+          url="https://linkedin.com/in/jardelgoncalves"
+          target="_blank"
+        />
+
+        <SocialButton icon="email" url="mailto:jardelgoncalves1996@gmail.com" />
+      </S.SocialList>
+    </S.PresentationContainer>
+  )
+}
---
+++ src/components/presentation/home/Presentation/styles.ts
@@ -0,0 +1,78 @@
+import styled, { keyframes } from 'styled-components'
+
+const GradientAnimation = keyframes`
+  from {
+    background-position-x: 0%;
+  };
+  to {
+    background-position-x: 400vw;
+  }
+`
+
+export const PresentationContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+export const TitleColorize = styled.h1`
+  margin-top: 12rem;
+  background: ${({ theme }) =>
+    `repeating-linear-gradient(${theme.colors.gradientFontColorize})`};
+  background-size: 200% auto;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-size: 400vw 400vw;
+  animation: ${GradientAnimation} 50s infinite linear forwards;
+  -webkit-animation: ${GradientAnimation} 50s infinite linear forwards;
+  font-size: 3.5rem;
+  font-weight: ${({ theme }) => theme.typography.weight.black};
+`
+
+export const Description = styled.p`
+  font-size: 1rem;
+  font-weight: ${({ theme }) => theme.typography.weight.light};
+  text-align: center;
+  line-height: ${({ theme }) => theme.typography.lineHeight.base};
+  color: ${({ theme }) => theme.colors.textColor};
+  max-width: 750px;
+  width: 100%;
+  padding: 0.75rem 0;
+
+  strong {
+    font-weight: ${({ theme }) => theme.typography.weight.black};
+    z-index: 3;
+
+    &:last-child {
+      position: relative;
+      padding: 0 0.75rem;
+      margin-left: 0.125rem;
+
+      &::after {
+        content: '';
+        position: absolute;
+        top: -2px;
+        left: 0;
+        width: 100%;
+        height: 1.625rem;
+        background: ${({ theme }) =>
+          `linear-gradient(${theme.colors.gradientEnterprise})`};
+        opacity: 0.6;
+        border-radius: 1.25rem;
+        z-index: -1;
+      }
+    }
+  }
+`
+
+export const SocialList = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 1.5rem;
+
+  a + a {
+    margin-left: 0.75rem;
+  }
+`
