29f7cd94b1af64da3a05ace04a43a703d0ad24cd
jardel <jardelgoncalves1996@gmail.com>
2021-06-11 22:41:06 -0300

    refactor: move logic to presentation components

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,31 +1,10 @@
-import { SocialButton } from '../../components/core/Button'
-
+import Presentation from 'components/presentation/home/Presentation'
 import * as S from './styles'
 
-export default function Home() {
+export default function Index() {
   return (
-    <S.PresentationContainer>
-      <S.TitleColorize>Hey, eu sou o Jardel</S.TitleColorize>
-      <S.Description>
-        Eu sou desenvolvedor de software, moro em Quixadá/Brasil. Eu atualmente
-        desenvolvo aplicações Web e Mobile em projetos pessoais e trabalho como
-        desenvolvedor <strong>Front-End</strong> na <strong>Ioasys</strong>
-      </S.Description>
-      <S.SocialList>
-        <SocialButton
-          icon="github"
-          url="https://github.com/jardelgoncalves"
-          target="_blank"
-        />
-
-        <SocialButton
-          icon="linkedin"
-          url="https://linkedin.com/in/jardelgoncalves"
-          target="_blank"
-        />
-
-        <SocialButton icon="email" url="mailto:jardelgoncalves1996@gmail.com" />
-      </S.SocialList>
-    </S.PresentationContainer>
+    <S.Container>
+      <Presentation />
+    </S.Container>
   )
 }
