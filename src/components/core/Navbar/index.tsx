aa58b9f9f2eaae26b686054943b36387ac0cd098
jardel <jardelgoncalves1996@gmail.com>
2021-06-13 12:08:43 -0300

    feat: using scroll indicator

--- src/components/core/Navbar/index.tsx
+++ src/components/core/Navbar/index.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Link from 'next/link'
 
 import { Layout } from 'components/core/Layout'
+import { ScrollIndicator } from 'components/core/ScrollIndicator'
 
 import * as S from './styles'
 
@@ -28,35 +29,42 @@ export const Navbar = ({ children }: NavbarProps) => {
   }, [lastYPos])
 
   return (
-    <S.Container
-      initial={{ opacity: 0 }}
-      animate={{ opacity: shouldShowNavbar ? 1 : 0 }}
-      transition={{ opacity: { duration: 0.2 } }}
-      withBackdrop={lastYPos > 30}
-    >
-      <Layout>
-        <S.NavWrapper>
-          <img src="/images/logo.svg" alt="Logo" />
-          <S.NavMenu>
-            <S.NavMenuItem>
-              <Link href="/">Página Inicial</Link>
-            </S.NavMenuItem>
-            <S.NavMenuItem>
-              <Link href="#">Artigos</Link>
-            </S.NavMenuItem>
-            <S.NavMenuItem>
-              <Link href="/about">Sobre mim</Link>
-            </S.NavMenuItem>
-            <S.NavMenuItem>
-              <Link href="#">RSS Feed</Link>
-            </S.NavMenuItem>
-            {/* <S.NavMenuItem>
+    <>
+      <S.Container
+        initial={{ opacity: 0 }}
+        animate={{ opacity: shouldShowNavbar ? 1 : 0 }}
+        transition={{ opacity: { duration: 0.2 } }}
+        withBackdrop={lastYPos > 30}
+      >
+        <Layout>
+          <S.NavWrapper>
+            <img src="/images/logo.svg" alt="Logo" />
+            <S.NavMenu>
+              <S.NavMenuItem>
+                <Link href="/">Página Inicial</Link>
+              </S.NavMenuItem>
+              <S.NavMenuItem>
+                <Link href="#">Artigos</Link>
+              </S.NavMenuItem>
+              <S.NavMenuItem>
+                <Link href="/about">Sobre mim</Link>
+              </S.NavMenuItem>
+              <S.NavMenuItem>
+                <Link href="#">RSS Feed</Link>
+              </S.NavMenuItem>
+              {/* <S.NavMenuItem>
               <Link href="#">AMA</Link>
             </S.NavMenuItem> */}
-          </S.NavMenu>
-          {children}
-        </S.NavWrapper>
-      </Layout>
-    </S.Container>
+            </S.NavMenu>
+            {children}
+          </S.NavWrapper>
+        </Layout>
+      </S.Container>
+      <ScrollIndicator
+        isVisible={lastYPos > 30}
+        position="fixed"
+        topDistance={shouldShowNavbar ? 70 : 0}
+      />
+    </>
   )
 }
