c0cac09e8662888045a86923651dca6bced84c6c
jardel <jardelgoncalves1996@gmail.com>
2021-06-15 23:38:18 -0300

    feat: add padding left and right

--- src/containers/Home/styles.ts
+++ src/containers/Home/styles.ts
@@ -3,4 +3,9 @@ import styled from 'styled-components'
 export const Container = styled.main`
   position: relative;
   width: 100%;
+  padding: 0 2.5rem;
+
+  ${({ theme }) => theme.breakpoints.lessThan('medium')`
+      padding: 0 0;
+  `}
 `
