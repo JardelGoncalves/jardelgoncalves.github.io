1f3a361214f2be08e79de300ffebf9ec7a359810
jardel <jardelgoncalves1996@gmail.com>
2021-06-17 00:38:11 -0300

    feat: add property to remove paddings

--- src/components/core/Layout/styles.ts
+++ src/components/core/Layout/styles.ts
@@ -1,13 +1,16 @@
 import styled from 'styled-components'
 
+type LayoutProps = {
+  full?: boolean | undefined
+}
+
 export const Wrapper = styled.main`
   display: flex;
   justify-content: center;
   width: 100%;
-  padding: 0 6.25rem;
 
   ${({ theme }) => theme.breakpoints.lessThan('huge')`
-    padding: 0 4.1875rem;
+    padding: 0 5.1875rem;
   `}
   ${({ theme }) => theme.breakpoints.lessThan('large')`
     padding: 0 3.1875rem;
@@ -20,7 +23,8 @@ export const Wrapper = styled.main`
   `}
 `
 
-export const Content = styled.section`
+export const Content = styled.section<LayoutProps>`
+  padding: ${({ full }) => (full ? 0 : '5rem')} 0;
   display: flex;
   max-width: ${({ theme }) => theme.sizes.portView};
   width: 100%;
