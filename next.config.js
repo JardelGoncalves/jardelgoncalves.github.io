3a36bf5a84b5b56a807e8a50949aa67cf0a558c0
jardel <jardelgoncalves1996@gmail.com>
2021-06-18 01:25:30 -0300

    chore: delete static folder

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/
-      },
-      use: ['@svgr/webpack']
-    })
-
-    return config
-  }
+  webpack5: true
 }
