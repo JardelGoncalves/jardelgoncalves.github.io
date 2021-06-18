b0728d097920d6215d584a9111b76b6c6d24d5e1
jardel <jardelgoncalves1996@gmail.com>
2021-06-18 01:27:11 -0300

    feat: add moon svg  to component

---
+++ src/components/core/Icon/Moon.tsx
@@ -0,0 +1,18 @@
+function Moon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="15"
+      height="15"
+      fill="none"
+      viewBox="0 0 15 15"
+    >
+      <path
+        fill="#FFDF5D"
+        d="M13.44 8.869A6.093 6.093 0 116.132 1.56a.469.469 0 01.561.561 5.161 5.161 0 006.185 6.185.469.469 0 01.562.562z"
+      ></path>
+    </svg>
+  )
+}
+
+export default Moon
