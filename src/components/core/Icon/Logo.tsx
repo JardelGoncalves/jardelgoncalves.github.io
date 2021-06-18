91ca876249df5cfcce22848fc9c7106268675b47
jardel <jardelgoncalves1996@gmail.com>
2021-06-18 01:26:53 -0300

    feat: add logo svg  to component

---
+++ src/components/core/Icon/Logo.tsx
@@ -0,0 +1,50 @@
+function Logo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="55"
+      height="55"
+      fill="none"
+      viewBox="0 0 55 55"
+    >
+      <path
+        stroke="url(#paint0_linear)"
+        d="M23.75 2.742a7.5 7.5 0 017.5 0l15.816 9.132a7.5 7.5 0 013.75 6.495V36.63a7.5 7.5 0 01-3.75 6.495L31.25 52.258a7.5 7.5 0 01-7.5 0L7.934 43.126a7.5 7.5 0 01-3.75-6.495V18.37a7.5 7.5 0 013.75-6.495L23.75 2.742z"
+      ></path>
+      <path
+        fill="url(#paint1_linear)"
+        fillRule="evenodd"
+        d="M35.864 16a7.378 7.378 0 00-7.378 7.378v7.905h.032a3.014 3.014 0 01-.145.716 2.39 2.39 0 01-.544.872.15.15 0 11-.22-.205c.225-.24.38-.475.48-.765.102-.292.151-.65.151-1.145h-1.862A7.378 7.378 0 0019 38.134h9.486a7.379 7.379 0 007.36-6.85h.018v-.51V16z"
+        clipRule="evenodd"
+      ></path>
+      <defs>
+        <linearGradient
+          id="paint0_linear"
+          x1="27.5"
+          x2="27.5"
+          y1="0"
+          y2="55"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#007EFF"></stop>
+          <stop offset="0.599" stopColor="#2AC769"></stop>
+          <stop offset="1" stopColor="#FB4E4E"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear"
+          x1="27.432"
+          x2="27.432"
+          y1="16"
+          y2="38.134"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#007EFF"></stop>
+          <stop offset="0.635" stopColor="#2AC769"></stop>
+          <stop offset="1" stopColor="#FB4E4E"></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export default Logo
