c9dc6032c9c32ebeb0207a5a678fb2ab6c36edfd
jardel <jardelgoncalves1996@gmail.com>
2021-06-20 17:52:44 -0300

    feat: add input component

---
+++ src/components/core/Input/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import * as S from './styles'
+
+interface InputProps {
+  label?: string
+  type?: string
+  value?: string
+  name?: string
+  placeholder?: string
+  width?: number
+  onChange?: () => void
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ label, width, ...props }, ref) => (
+    <S.Wrapper width={width}>
+      {label && <S.Label>{label}</S.Label>}
+      <S.Input ref={ref} {...props} />
+    </S.Wrapper>
+  )
+)
+
+Input.displayName = 'Input'
+export { Input }
