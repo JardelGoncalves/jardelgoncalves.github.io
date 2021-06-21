c87472ada51f1bfbc6969c5887e5d06c6af20989
jardel <jardelgoncalves1996@gmail.com>
2021-06-20 22:26:21 -0300

    feat: add raised butto  to page

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,14 +1,18 @@
 import { Input } from 'components/core/Input'
 import { Form } from 'components/core/Form'
 import * as S from './styles'
+import { RaisedButton } from 'components/core/Button'
 
 export default function Login() {
   return (
     <S.Container>
       <Form width={600}>
-        <S.Title>Faca login para continuar</S.Title>
-        <Input placeholder="Email" />
+        <S.Title>Faça login para continuar</S.Title>
+        <Input placeholder="Email" type="email" />
         <Input placeholder="Senha" type="password" />
+        <S.ButtonWrapper>
+          <RaisedButton label="Entrar" width={200} />
+        </S.ButtonWrapper>
       </Form>
     </S.Container>
   )
