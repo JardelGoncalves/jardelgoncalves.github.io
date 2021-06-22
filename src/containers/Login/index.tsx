09de758723016fd34e9ee8527a50a7ea20969b15
jardel <jardelgoncalves1996@gmail.com>
2021-06-21 23:41:03 -0300

    feat: add hooks to login container

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,17 +1,24 @@
+import { useForm } from 'react-hook-form'
+
 import { Input } from 'components/core/Input'
 import { Form } from 'components/core/Form'
-import * as S from './styles'
 import { RaisedButton } from 'components/core/Button'
 
+import * as S from './styles'
+
 export default function Login() {
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = handleSubmit((data) => console.log(data))
+
   return (
     <S.Container>
-      <Form width={600}>
+      <Form width={600} onSubmit={onSubmit}>
         <S.Title>Faça login para continuar</S.Title>
-        <Input placeholder="Email" type="email" />
-        <Input placeholder="Senha" type="password" />
+        <Input {...register('email')} placeholder="Email" type="email" />
+        <Input {...register('password')} placeholder="Senha" type="password" />
         <S.ButtonWrapper>
-          <RaisedButton label="Entrar" width={200} />
+          <RaisedButton type="submit" label="Entrar" width={200} />
         </S.ButtonWrapper>
       </Form>
     </S.Container>
