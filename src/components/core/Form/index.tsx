f7b974e5defdb7210b06643b0737a165d14fccf0
jardel <jardelgoncalves1996@gmail.com>
2021-06-21 23:41:18 -0300

    feat: add onSubmit event

--- src/components/core/Form/index.tsx
+++ src/components/core/Form/index.tsx
@@ -3,8 +3,13 @@ import * as S from './styles'
 interface FormProps {
   width?: number
   children: React.ReactNode
+  onSubmit?: () => void
 }
 
-export const Form = ({ children, width }: FormProps) => {
-  return <S.WrapperForm width={width}>{children}</S.WrapperForm>
+export const Form = ({ children, width, onSubmit }: FormProps) => {
+  return (
+    <S.WrapperForm onSubmit={onSubmit} width={width}>
+      {children}
+    </S.WrapperForm>
+  )
 }
