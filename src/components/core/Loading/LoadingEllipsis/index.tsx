0c5bef332e349a637e41258486659259a5859f22
jardel <jardelgoncalves1996@gmail.com>
2021-06-21 23:40:12 -0300

    feat: add loading ellipsis component

---
+++ src/components/core/Loading/LoadingEllipsis/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles'
+
+export const LoadingEllipsis = () => {
+  return (
+    <S.EllipsisContainer>
+      <div />
+      <div />
+      <div />
+      <div />
+    </S.EllipsisContainer>
+  )
+}
