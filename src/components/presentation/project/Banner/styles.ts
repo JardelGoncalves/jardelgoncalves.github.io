82d4d34f08227bda1c3ec8e38a3e167da5ffc423
jardel <jardelgoncalves1996@gmail.com>
2021-06-26 20:51:00 -0300

    feat: add new style content

--- src/components/presentation/project/Banner/styles.ts
+++ src/components/presentation/project/Banner/styles.ts
@@ -1,12 +1,29 @@
+import { rgba } from 'polished'
 import styled from 'styled-components'
 
-export const BannerWrapper = styled.header`
+export const Content = styled.header`
+  width: 100%;
+  height: 95vh;
+  display: flex;
+  transition: background-color 0.4s;
+  background-color: ${({ theme }) => rgba(theme.colors.bannerProject, 0.4)};
+
+  .scroll-to-anchor {
+    position: absolute;
+    bottom: 4rem;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`
+
+export const BannerWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   width: 100%;
-  padding: 5rem 0;
-  border-radius: 1rem;
-  margin-top: 2rem;
+  height: 100%;
 `
 
 export const LogoWrapper = styled.div`
@@ -20,7 +37,7 @@ export const LogoWrapper = styled.div`
 
 export const Title = styled.h1`
   font-size: ${({ theme }) => theme.typography.sizes['2xl']};
-  font-weight: ${({ theme }) => theme.typography.weight.xBold};
+  font-weight: ${({ theme }) => theme.typography.weight.bold};
   text-align: center;
 `
 
@@ -28,4 +45,5 @@ export const Description = styled.div`
   margin-top: 0.5rem;
   text-align: center;
   color: ${({ theme }) => theme.colors.description};
+  font-weight: ${({ theme }) => theme.typography.weight.regular};
 `
