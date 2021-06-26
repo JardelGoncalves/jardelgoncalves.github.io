ff5f2f6e578503f542931abc46d95180ab671708
jardel <jardelgoncalves1996@gmail.com>
2021-06-26 12:51:54 -0300

    refactor: change description color

--- src/styles/tokens/colors/dark.ts
+++ src/styles/tokens/colors/dark.ts
@@ -2,12 +2,9 @@ import { Colors, base } from './base'
 export const dark: Colors = base({
   background: '#0E141B',
   text: '#FAFAFA',
-  description: '#737373',
+  description: '#DBDDE0',
   border: '#e4e4e71a',
   textMenu: '#FAFAFA',
   toggleBorder: 'rgba(0, 126, 255, 0.3)',
-  toggleBackground: 'rgba(0, 126, 255, 0.2)',
-  descriptionColor: '#DBDDE0'
+  toggleBackground: 'rgba(0, 126, 255, 0.2)'
 })
-
-// toggleBackgroundContainer: 'transparent',
