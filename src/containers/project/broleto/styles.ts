074253353b73b2bbc0d56b1bb867b2ce94629427
jardel <jardelgoncalves1996@gmail.com>
2021-06-26 20:53:18 -0300

    feat: add styles to banner and sections docs

--- src/containers/project/broleto/styles.ts
+++ src/containers/project/broleto/styles.ts
@@ -1,7 +1,23 @@
 import styled from 'styled-components'
 
-export const Container = styled.main`
+export const Wrapper = styled.main`
   display: flex;
   flex-direction: column;
   width: 100%;
+
+  .grid-template-columns {
+    margin-top: 5rem;
+  }
+`
+export const Container = styled.section`
+  width: 100%;
+  padding: 0 3rem;
+`
+
+export const Side = styled.section`
+  width: 100%;
+
+  @media screen and (max-width: 900px) {
+    display: none;
+  }
 `
