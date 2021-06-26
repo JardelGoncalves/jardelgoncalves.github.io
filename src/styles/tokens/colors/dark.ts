0b549719272e2bf342748fb80d5027dac683ba87
jardel <jardelgoncalves1996@gmail.com>
2021-06-26 20:51:41 -0300

    refactor: change background, title and bannerProject colors

--- src/styles/tokens/colors/dark.ts
+++ src/styles/tokens/colors/dark.ts
@@ -1,10 +1,12 @@
 import { Colors, base } from './base'
 export const dark: Colors = base({
-  background: '#0E141B',
+  background: '#121214',
   text: '#FAFAFA',
   description: '#DBDDE0',
   border: '#e4e4e71a',
   textMenu: '#FAFAFA',
   toggleBorder: 'rgba(0, 126, 255, 0.3)',
-  toggleBackground: 'rgba(0, 126, 255, 0.2)'
+  toggleBackground: 'rgba(0, 126, 255, 0.2)',
+  title: '#FFE100',
+  bannerProject: '#141417'
 })
