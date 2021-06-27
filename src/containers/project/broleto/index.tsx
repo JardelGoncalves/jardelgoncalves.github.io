5ec00d24fbd517089407fa496a8f2937fa14ce11
jardel <jardelgoncalves1996@gmail.com>
2021-06-27 13:52:44 -0300

    feat: add syntax code to installation

--- src/containers/project/broleto/index.tsx
+++ src/containers/project/broleto/index.tsx
@@ -2,13 +2,14 @@ import { nanoid } from 'nanoid'
 
 import { Banner } from 'components/presentation/project/Banner'
 import { Grid } from 'components/core/Grid/'
-import { TableContent } from 'components/core/TableContent/index'
 import { Title } from 'components/core/Title'
-import { Layout } from 'components/core/Layout/index'
+import { Layout } from 'components/core/Layout'
+import { TableContent } from 'components/core/TableContent'
 
 import * as S from './styles'
 
 import broletoMock from 'static/mocks/broleto.json'
+import { Sintax } from 'components/core/Syntax'
 
 export default function Broleto() {
   return (
@@ -22,19 +23,46 @@ export default function Broleto() {
         <S.WrapperContainer>
           <Grid column="1fr 220px">
             <S.Container className="content">
-              {broletoMock.sections.map((section) => (
-                <S.Content id={section.id} key={nanoid()}>
-                  <Title
-                    size="xl"
-                    text={section.title}
-                    heading="h2"
-                    anchorId={section.id}
-                  />
-                  {section.descriptions.map((description) => (
+              <S.Content id={broletoMock.sections.introduction.id}>
+                <Title
+                  size="xl"
+                  text={broletoMock.sections.introduction.title}
+                  heading="h2"
+                  anchorId={broletoMock.sections.introduction.id}
+                />
+                {broletoMock.sections.introduction.descriptions.map(
+                  (description) => (
                     <S.Description key={nanoid()}>{description}</S.Description>
-                  ))}
-                </S.Content>
-              ))}
+                  )
+                )}
+              </S.Content>
+              <S.Content id={broletoMock.sections.installation.id}>
+                <Title
+                  size="xl"
+                  text={broletoMock.sections.installation.title}
+                  heading="h2"
+                  anchorId={broletoMock.sections.installation.id}
+                />
+                {broletoMock.sections.installation.descriptions.map(
+                  (description) => (
+                    <S.Description key={nanoid()}>{description}</S.Description>
+                  )
+                )}
+                <Sintax
+                  codes={[
+                    {
+                      label: 'JS/TS',
+                      language: 'bash',
+                      code: 'npm i broleto'
+                    },
+                    {
+                      label: 'Dart',
+                      language: 'yaml',
+                      code: `dependencies:\n  ...\n  broleto: ^0.0.11`
+                    }
+                  ]}
+                />
+              </S.Content>
             </S.Container>
             <S.Side className="content-table-section">
               <TableContent
