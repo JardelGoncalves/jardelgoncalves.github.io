fdc4937ce934c03bae91e0cb8d5bef241f374573
jardel <jardelgoncalves1996@gmail.com>
2021-06-27 13:45:19 -0300

    chore: configure transpile to syntax highlghter

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 // eslint-disable-next-line no-undef
-module.exports = {
+const withPlugins = require('next-compose-plugins')
+const withTM = require('next-transpile-modules')
+
+const plugins = [
+  [
+    withTM,
+    {
+      transpileModules: ['react-syntax-highlighter']
+    }
+  ]
+]
+
+const nextConfig = {
   webpack5: true
 }
+
+module.exports = withPlugins([...plugins], nextConfig)
