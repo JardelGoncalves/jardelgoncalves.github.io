dfe613c20c226c38526729dd871e199a48ff3ac1
jardel <jardelgoncalves1996@gmail.com>
2021-07-02 15:31:13 -0300

    refactor: add async to methods

--- src/lib/utils/hash.ts
+++ src/lib/utils/hash.ts
@@ -1,11 +1,11 @@
 import bcrypt from 'bcrypt'
 
 export class Hash {
-  static generate(text: string, rounds = 10) {
+  static async generate(text: string, rounds = 10) {
     return bcrypt.hashSync(text, rounds)
   }
 
-  static compare(text: string, encrypted: string) {
+  static async compare(text: string, encrypted: string) {
     return bcrypt.compareSync(text, encrypted)
   }
 }
