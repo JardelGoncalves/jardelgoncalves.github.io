0cb6369cefc9b9f22de38a5a43500fabe7ce904d
jardel <jardelgoncalves1996@gmail.com>
2021-07-02 19:02:43 -0300

    refactor: change connection with database

--- src/lib/middlewares/connection-middleware.ts
+++ src/lib/middlewares/connection-middleware.ts
@@ -2,15 +2,16 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { NextFunction } from 'express'
 
 import * as database from '../database'
-global.mongo = global.mongo || null
+global.mongo = global.mongo || {}
 
 export const connection = async (
   _: NextApiRequest,
   _res: NextApiResponse,
   next: NextFunction
 ) => {
-  if (!global.mongo) {
-    global.mongo = await database.connect()
+  if (!global.mongo.isConnected) {
+    const db = await database.connect()
+    global.mongo.isConnected = db.connections[0].readyState;
   }
   return next()
 }
