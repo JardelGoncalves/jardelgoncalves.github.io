08f5d8a0405f9432d3b2cf1a52cd4a77de7bd815
jardel <jardelgoncalves1996@gmail.com>
2021-07-10 16:59:00 -0300

    feat: add watch to email and password for enabled/disabled button

--- src/containers/login/index.tsx
+++ src/containers/login/index.tsx
@@ -1,4 +1,4 @@
-import { useForm } from 'react-hook-form'
+import { useForm, useWatch } from 'react-hook-form'
 
 import { Input } from 'components/core/Input'
 import { Form } from 'components/core/Form'
@@ -7,7 +7,10 @@ import { RaisedButton } from 'components/core/Button'
 import * as S from './styles'
 
 export default function Login() {
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, control } = useForm()
+
+  const email = useWatch({ control, name: 'email', defaultValue: '' })
+  const password = useWatch({ control, name: 'password', defaultValue: '' })
 
   const onSubmit = handleSubmit((data) => console.log(data))
 
@@ -15,10 +18,23 @@ export default function Login() {
     <S.Container>
       <Form width={600} onSubmit={onSubmit}>
         <S.Title>Faça login para continuar</S.Title>
-        <Input {...register('email')} placeholder="Email" type="email" />
-        <Input {...register('password')} placeholder="Senha" type="password" />
+        <Input
+          {...register('email', { required: true })}
+          placeholder="Email"
+          type="email"
+        />
+        <Input
+          {...register('password', { required: true })}
+          placeholder="Senha"
+          type="password"
+        />
         <S.ButtonWrapper>
-          <RaisedButton type="submit" label="Entrar" width={200} />
+          <RaisedButton
+            disabled={!(email && password)}
+            type="submit"
+            label="Entrar"
+            width={200}
+          />
         </S.ButtonWrapper>
       </Form>
     </S.Container>
