b27da086ce5d9a6519fcfee58d3abc95726159a2
jardel <jardelgoncalves1996@gmail.com>
2021-07-14 23:27:39 -0300

    refactor: add minus prop to choosen style

--- src/components/core/Layout/Side/index.tsx
+++ src/components/core/Layout/Side/index.tsx
@@ -1,5 +1,14 @@
 import * as S from './styles'
+import { SideItem } from './SideItem/index'
 
-export const Side = () => {
-  return <S.Wrapper></S.Wrapper>
+interface SideProps {
+  minus?: boolean
+}
+
+export const Side = ({ minus }: SideProps) => {
+  return (
+    <S.Wrapper>
+      <SideItem noHover minus={minus} icon="logo" />
+    </S.Wrapper>
+  )
 }
