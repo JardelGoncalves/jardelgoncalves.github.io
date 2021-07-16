eda665afd0f5ba64ebad66cd9e806c412cfc9550
jardel <jardelgoncalves1996@gmail.com>
2021-07-16 00:00:35 -0300

    refactor: change svg color

--- src/components/core/Layout/Side/styles.ts
+++ src/components/core/Layout/Side/styles.ts
@@ -4,5 +4,27 @@ export const Wrapper = styled.nav`
   grid-area: side;
   height: 100%;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   padding-left: 16px;
+`
+
+export const ItemsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  svg {
+    fill: ${({ theme }) => theme.colors.text};
+  }
+`
+
+export const LogoutButton = styled.button`
+  border: none;
+  background-color: transparent;
+  color: ${({ theme }) => theme.colors.text};
+  width: 100%;
+  svg {
+    fill: ${({ theme }) => theme.colors.text};
+  }
 `
